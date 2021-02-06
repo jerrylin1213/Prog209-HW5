6d96d3ab1c43d0896b2a09bdea58cfb98543e741
hughle68 <77129962+hughle68@users.noreply.github.com>
2021-02-06 01:18:20 -0800

    Add files via upload

--- Index.js
+++ Index.js
@@ -1,52 +1,48 @@
 let movieTitle = document.getElementById("title");
 let movieRating = document.getElementById("rating");
-// let movieList = ["Movie Title", "Movie Rating"];  // Hugh commented
-let movieList = [];   // Hugh added
+let movieCount = document.getElementById("count");
+let movieList = [];
+movieTitle.focus();
+
 
 function addMovie() {
-    if (movieTitle.value == "") {
-        alert("Please enter a movie title.");
-    } else if (isNaN(movieRating.value) || movieRating.value == "") {
-        alert("Please enter number for rating.");
-    } else if (parseInt(movieRating.value) < 1 || parseInt(movieRating.value) > 5) {
-        alert("Please enter a rating between \"1\" and \"5\".");
-    }
-    else {
-        
-        let movie_inp = movieTitle.value;       // Hugh added
-        let rating_inp = movieRating.value;     // Hugh added
-        let Movie = new MovieObj(movie_inp, rating_inp);  // Hugh added
-        movieList.push(Movie);  // Hugh added
-        // movieList.push(movieTitle, movieRating);  // Hugh commented
-    }
-    // clear out the 2 input boxes and refocus on movieTitle. Hugh added
-    movieTitle.value = "";   // Hugh added
-    movieRating.value = "";  // Hugh added
-    movieTitle.focus();      // Hugh added
+  if (movieTitle.value == "") {
+      alert("Please enter a movie title.");
+  } else if (isNaN(movieRating.value) || movieRating.value == "") {
+      alert("Please enter number for rating.");
+  } else if (parseInt(movieRating.value) < 1 || parseInt(movieRating.value) > 5) {
+      alert("Please enter a rating between \"1\" and \"5\".");
+  }
+  else {  
+      let movie_inp = movieTitle.value;
+      let rating_inp = movieRating.value;
+      let Movie = new MovieObj(movie_inp, rating_inp);
+      movieList.push(Movie);
+      // show number of movies
+      movieCount.innerHTML = " " + movieList.length + " " + " movie(s)";
+  }
+  // clear out the 2 input boxes and refocus on movieTitle.
+  movieTitle.value = "";
+  movieRating.value = "";
+  movieTitle.focus();
 }
 
-// Hugh commented
-// function showMovie() {
-//     movieList += document.getElementsByName("input");
-//     document.getElementById("result").innerHTML += movieList.length;
-
-//     for (let i = 0; i < movieList.length; i++) {
-//         // document.getElementById("result").innerHTML += movieList[i].value + " ";
-//     }
-// }
-
-// Hugh added this function
 function showMovie()
 {
   if (movieList.length > 0)
   {
-    // sortByRatings();
+    sortByRatings();   // sort movies by ratings
+    let str = "Sort Movies by Ratings (good to bad):" + "<br>";  //sort movies by ratings
 
-    let str = "Movies and Ratings:" + "<br><br>";
+    //let str = "Movies and Ratings:" + "<br><br>";  // list movies and ratings
     for (let i=0; i < movieList.length; i++) {
       str += movieList[i].toString();
     }
 
     document.getElementById("result").innerHTML = str;
   }
+}
+
+function sortByRatings() {
+  movieList.sort( (a,b) => { return b.rating - a.rating; } );
 }
--- movie.js
+++ movie.js
@@ -1,15 +1,18 @@
-
 // movie object constructor
 let MovieObj = function (title, rating) {
   this.title = title;
-  this.rating = rating; 
-  this.validate = function() { 
+  this.rating = rating;
+  this.validate = function () {
     if ((this.title !== "") && (this.rating !== "")) {
       return true;
     }
     return false;
   };
-  this.toString = function() {
+  this.toString = function () {
+    // list movies and ratings
+    //return this.title + "&nbsp &nbsp &nbsp &nbsp &nbsp" + this.rating + "<br>";
+    
+    // sort movies by ratings
     return this.rating + "&nbsp &nbsp &nbsp &nbsp &nbsp" + this.title + "<br>";
   };
 };
